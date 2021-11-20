0c02326d3013353096e77f26f6a43ca499e66028
scto <tschmid35@gmail.com>
2021-11-20 12:08:46 +0100

    r8q: Add OTA Assert for original codenames
    to support recoveries with other codenames

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,8 @@ TARGET_KERNEL_CONFIG            := vendor/kona_sec_custom_scto_defconfig
 TARGET_KERNEL_VARIANT_DEFCONFIG := vendor/r8q/kona_sec_r8q_eur_defconfig
 KERNEL_LD                       := DEBUG_DEFCONFIG=vendor/release_defconfig
 
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := r8q
+
 # Inherit from the proprietary version
 include vendor/samsung/r8q/BoardConfigVendor.mk
